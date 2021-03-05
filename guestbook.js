797e59ac2d1e8cdbf1ca3dbe891e45ad8bf658ca
CST44 <78963459+CST44@users.noreply.github.com>
2021-03-05 22:11:21 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `https://1f05929a.us-south.apigw.appdomain.cloud/guestbook/entries`,
       dataType: 'json'
     });
   },
