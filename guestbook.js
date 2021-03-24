ca6649ae82e96626286c78136670b69fe86a66c5
NowsGT <nowsath_uom@ymail.com>
2021-03-24 12:10:01 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ce16a00e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
