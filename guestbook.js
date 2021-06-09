e48c90471e3f36c182b391b1d73027d3ce6f6011
Nonso-J <81563469+Nonso-J@users.noreply.github.com>
2021-06-09 17:31:01 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e409bd15.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
