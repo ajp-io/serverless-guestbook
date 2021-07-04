d55e4da2018df8898bcc9a4611e7393c4099cb61
Sumeet2013 <86927358+Sumeet2013@users.noreply.github.com>
2021-07-04 23:42:23 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://github.com/Sumeet2013/serverless-guestbook/blob/main/guestbook.js';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
