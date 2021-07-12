3759d1d26b7b6116f05ff007a76d7963569583a1
Diego Monteiro Paes Martos <66971181+emardie@users.noreply.github.com>
2021-07-12 18:55:59 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4bd2cb98.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
