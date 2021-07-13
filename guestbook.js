a957e4add1d2a702536c45b671fa97e36afe1126
SergeyPo <mail_3@mail.ru>
2021-07-13 21:28:02 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://fc7531a0.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
