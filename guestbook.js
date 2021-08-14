4757cb969f6f6a04c4094e6778e1593740e6a9f8
raghuvir-naik <79088829+raghuvir-naik@users.noreply.github.com>
2021-08-14 15:25:03 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a3bbe565.jp-tok.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
