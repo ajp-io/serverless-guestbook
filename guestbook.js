553c0bdf895cd066dcaed4a6bc7531ee66c448fe
jothibossm <89164565+jothibossm@users.noreply.github.com>
2021-08-19 00:27:45 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9d9e602d.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
