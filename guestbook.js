06743372efb7a2ae50ba27608ba146b5641503cc
echrige <73955964+echrige@users.noreply.github.com>
2021-08-31 19:02:38 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7dd4971f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
