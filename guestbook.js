ca60ec8f4a21e0533849fbe20e51d14c29598433
gyan2908 <65892684+gyan2908@users.noreply.github.com>
2021-11-20 14:27:16 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1f35e8db.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
