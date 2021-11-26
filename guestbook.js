f0edc4d7fcf793381a43d82e32ef3e86327809c7
emmanuelnegash <shalovmine@gmail.com>
2021-11-26 11:40:31 +0300

    Guestbook API route

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://81901744.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
