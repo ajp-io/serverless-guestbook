7f9590a5a6c1b0c0c5f17907c58aae39ef9dc893
ikettunen <45572086+ikettunen@users.noreply.github.com>
2022-01-24 20:06:52 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c505f018.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
