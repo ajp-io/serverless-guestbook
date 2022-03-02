a105ee2bbf84f518baa1fc34a8997caa5e883f68
swapnil1505 <54595442+swapnil1505@users.noreply.github.com>
2022-03-02 12:31:46 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://78658d5b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
