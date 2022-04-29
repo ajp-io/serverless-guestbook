fe1e3567579058e4f40740767d43770dd95d6ba5
Pippin1901 <95875674+Pippin1901@users.noreply.github.com>
2022-04-29 16:37:07 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9ca8887e.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
