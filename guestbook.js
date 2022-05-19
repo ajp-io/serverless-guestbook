0c35190b2f786518253c32d2084ae2b2de635a04
lotfy2125 <99709090+lotfy2125@users.noreply.github.com>
2022-05-19 04:47:14 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d46c795b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
