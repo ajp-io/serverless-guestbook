ecbeba14f255046214bfb149ff1c65a5cccc403c
albertlse11 <albertlse@gmail.com>
2022-05-20 21:52:34 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://305cccfe.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
