3fc1e277ea634ee43cdf7b3f5ac6217fe445e3cc
tarony33 <105033607+tarony33@users.noreply.github.com>
2022-07-23 17:46:53 +1000

    Update guestbook.js
    Added Route API

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3c420057.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
