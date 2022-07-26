21fd234b8cb29dbf4436e621981805e048c78fdf
Shivi167 <109944022+Shivi167@users.noreply.github.com>
2022-07-26 08:39:09 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b1ce7219.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
