356a54840af5424a541d286ffd8398f9ae21ca54
fuadmelhi <50489814+fuadmelhi@users.noreply.github.com>
2022-07-29 12:21:50 +0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1486b734.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
