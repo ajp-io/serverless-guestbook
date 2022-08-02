298580d71179473ca2ed3a4721375ce1bfad9dc4
TreyMurray01 <109750453+TreyMurray01@users.noreply.github.com>
2022-08-02 12:42:16 -0400

    Update api URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://17bdd34a.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
