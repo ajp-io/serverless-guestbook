039b0fb3dee97a148f1cb83ebf8765a805fdddc6
mortezabonak <33400847+mortezabonak@users.noreply.github.com>
2022-08-03 18:48:47 +0430

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://62f7fbdc.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
