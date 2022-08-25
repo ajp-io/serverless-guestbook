a0c565c8fca6cae2243eb8dace7da1ed37e279d3
SamehSaber1 <108372083+SamehSaber1@users.noreply.github.com>
2022-08-25 19:22:17 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ae10460c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
