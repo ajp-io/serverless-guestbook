d09f9f1c815363005ead96c3505888af0bd66f43
ABHISHEK KUMAR VERMA <vermaabhishek357@gmail.com>
2022-12-31 03:41:33 +0100

    updates url route in guestbook.js
    Inserted the URL within the ' ' on line 4. Removed the name of the sequence from the end.
    
    For example, if the URL is https://eu-gb.functions.appdomain.cloud/api/v1/web/395da288-6950-435fffc27ddd/guestbook/read-guestbook-entries-sequence, then shorten the URL by removing the /read-guestbook-entries-sequence. So now your URL should look something like this:
    https://eu-gb.functions.appdomain.cloud/api/v1/web/395da288-6950-435fffc27ddd/guestbook
    
    Replaced the get method's URL endpoint /entries with /read-guestbook-entries-sequence.json (line 10) and for post method (line 19) with /save-guestbook-entry-sequence.json

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/9b7d5638-dc06-4834-b153-4a3b46359fca/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
